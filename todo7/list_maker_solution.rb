ae0cf7c1872fd341cb373e287cfd54bf0709d331
sranso <sranso@gmail.com>
2013-11-20 09:37:53 -0500

    add todo7/

---
+++ todo7/list_maker_solution.rb
@@ -0,0 +1,21 @@
+# Create a test for a method `make_list` that takes an array and returns that same array but
+# as an array of strings in a numbered list.
+
+# Run this test by typing `rspec list_maker_spec.rb` in your terminal.
+
+# Then, create a method on array called `make_list` that iterates over the array it is
+# called on and appends a number, a period, and a space to each element.
+
+# e.g make_list(["ich", "ni", "san"]) #=> ["1. ich", "2. ni", "3. san"]
+
+def make_list(array)
+	num = 0
+	array.collect! do |x|
+		while num <= array.length
+			"#{num}. #{x}"
+			num +=1
+		end
+	end
+end
+
+puts make_list(["sarah", "peeps", "forever"])
