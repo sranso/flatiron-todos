7093b84ffc36fc8e007021941092d527b3d46053
sranso <sranso@gmail.com>
2013-11-26 09:25:34 -0500

    rewrite spec to be my own tests

--- todo11/spec.rb
+++ todo11/spec.rb
@@ -1,16 +1,16 @@
 require_relative "./key for min value"
 
 describe "smallest hash value" do
-  it "should return the key of the smallest hash value" do
-    key_for_min_value({:blake => 500, :ashley => 2, :adam => 1}).should eq(:adam)
+  it "should return the smallest value from this hash" do
+    expect(key_for_min_value({:sarah => 3, :kate => 1, :ariel => 55})).to eq(:kate)
   end
 
-  it "should return the key of the smallest hash value example 2" do
-    key_for_min_value({:blake => 10, :ashley => 50, :adam => 17}).should eq(:blake)
+  it "should return the smallest value from this hash, take 2" do
+    expect(key_for_min_value({:gus => -5, :chris => -500, :nisha => 0})).to eq(:chris)
   end
 
   it "should return nil for an empty hash" do
-    key_for_min_value({}).should eq(nil)
+    expect(key_for_min_value({})).to eq(nil)
   end
 
 end
