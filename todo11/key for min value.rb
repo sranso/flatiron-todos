8738a7a2c66d14af19278d6b0a0be4115d9d8073
sranso <sranso@gmail.com>
2013-11-26 09:20:37 -0500

    test passes in todo11/

--- todo11/key for min value.rb
+++ todo11/key for min value.rb
@@ -1,5 +1,12 @@
+require "debugger"
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 def key_for_min_value(hash)
-  # code goes here
+  lowest_val = 10**10000
+  hash.each do |key1, value1|
+    lowest_val = value1 unless value1 > lowest_val
+  end
+  hash.key(lowest_val)
 end
+
+# p key_for_min_value({:blake => 500, :ashley => 2, :adam => 1})
