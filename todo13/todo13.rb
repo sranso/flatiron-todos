94e5bcdc283f582fa9eda06ff056e18c7cd0a377
sranso <sranso@gmail.com>
2013-12-03 18:26:45 -0500

    todo13 actually works now. spec included.

--- todo13/todo13.rb
+++ todo13/todo13.rb
@@ -5,12 +5,12 @@ class Array
     
   # end
 
-  def version_sort(orig_array)
+  def version_sort
     array_to_sort = []
     new_array = []
 
     #get data from files
-    orig_array.each do |item|
+    self.each do |item|
       array_to_sort << (/foo-(\d*)\.{0,1}(\d*)\.{0,1}(\d*)([a-z]{0,1})\.ext/).match(item)
     end
 
@@ -72,34 +72,3 @@ filenames = [
   "foo-25.ext",
   "foo-6.ext",
 ]
-
-test = Array.new
-test.version_sort(filenames)
-
-# version_sorted_filenames = [
-#   "foo-1.ext",
-#   "foo-1.3.ext",
-#   "foo-1.8.7.ext",
-#   "foo-1.9.3.ext",
-#   "foo-1.10.2.ext",
-#   "foo-1.11.ext",
-#   "foo-1.50.ext",
-#   "foo-2.0.ext",
-#   "foo-2.0a.ext",
-#   "foo-2.0b.ext",
-#   "foo-2.0.0.ext",
-#   "foo-2.0.1.ext",
-#   "foo-2.01.ext",
-#   "foo-2.1.ext",
-#   "foo-2.01a.ext",
-#   "foo-2.007.ext",
-#   "foo-2.012b.ext",
-#   "foo-6.ext",
-#   "foo-10.ext",
-#   "foo-10.1.ext",
-#   "foo-13.ext",
-#   "foo-25.ext",
-#   "foo-100.ext",
-# ]
-
-filenames.version_sort.should eq(version_sorted_filenames)
--- todo13/todo13_spec.rb
+++ todo13/todo13_spec.rb
@@ -1,34 +1,57 @@
-require_relative './todo13'
-
-describe Array do
-  describe '#version_sort' do
-    it "should" do
-      expect(test.version_sort). to eq()
-    end
+require "./todo13"
+require "rspec"
+describe "version sort" do
+  it "should" do
+    filenames = [
+      "foo-1.10.2.ext",
+      "foo-1.11.ext",
+      "foo-1.3.ext",
+      "foo-1.50.ext",
+      "foo-1.8.7.ext",
+      "foo-1.9.3.ext",
+      "foo-1.ext",
+      "foo-10.1.ext",
+      "foo-10.ext",
+      "foo-100.ext",
+      "foo-13.ext",
+      "foo-2.0.0.ext",
+      "foo-2.0.1.ext",
+      "foo-2.0.ext",
+      "foo-2.007.ext",
+      "foo-2.01.ext",
+      "foo-2.012b.ext",
+      "foo-2.01a.ext",
+      "foo-2.0a.ext",
+      "foo-2.0b.ext",
+      "foo-2.1.ext",
+      "foo-25.ext",
+      "foo-6.ext",
+    ]
+    version_sorted_filenames = [
+      "foo-1.ext",
+      "foo-1.3.ext",
+      "foo-1.8.7.ext",
+      "foo-1.9.3.ext",
+      "foo-1.10.2.ext",
+      "foo-1.11.ext",
+      "foo-1.50.ext",
+      "foo-2.0.ext",
+      "foo-2.0a.ext",
+      "foo-2.0b.ext",
+      "foo-2.0.0.ext",
+      "foo-2.0.1.ext",
+      "foo-2.01.ext",
+      "foo-2.1.ext",
+      "foo-2.01a.ext",
+      "foo-2.007.ext",
+      "foo-2.012b.ext",
+      "foo-6.ext",
+      "foo-10.ext",
+      "foo-10.1.ext",
+      "foo-13.ext",
+      "foo-25.ext",
+      "foo-100.ext",
+    ]
+    filenames.version_sort.should eq(version_sorted_filenames)
   end
 end
-
-# foo-1.ext
-# foo-1.3.ext
-# foo-1.8.7.ext
-# foo-1.9.3.ext
-# foo-1.10.2.ext
-# foo-1.11.ext
-# foo-1.50.ext
-# foo-2.0.ext
-# foo-2.0.0.ext @@
-# foo-2.0a.ext @@
-# foo-2.0b.ext @@
-# foo-2.0.1.ext @@
-# foo-2.1.ext @@
-# foo-2.01.ext @@
-# foo-2.01a.ext
-# foo-2.007.ext
-# foo-2.012b.ext
-# foo-6.ext
-# foo-10.ext
-# foo-10.1.ext
-# foo-13.ext
-# foo-25.ext
-# foo-100.ext
-
