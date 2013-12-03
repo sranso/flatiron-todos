18c8ecb92f22c99c9232fd0e8bdac8319fbad4aa
sranso <sranso@gmail.com>
2013-12-03 17:34:00 -0500

    use sort method with spaceships to sort array, todo13/

--- todo13/todo13.rb
+++ todo13/todo13.rb
@@ -1,39 +1,42 @@
 require 'debugger'
 class Array
-  def version_sort(array_to_sort)
-    #get data from files
-    array = []
-    array_to_sort.each do |item|
-      array << (/.*\-(\d*\.*\d*\w*\.*\d*\.*)ext/).match(item)[1]
-    end
 
-    #split items into arrays of arrays
-    array_of_arrays = []
-    array.each do |item|
-      array_of_arrays << item.split(".")
-    end
-
-    #make all arrays the same size
-    array_of_arrays.select do |array|
-      while array.size < 3
-        array << ""
-      end
-    end
-
-    sorted_array = array_of_arrays.sort do |a,b|
-#[["1", "", "", ""], ["1", "10", "2", ""], ["1", "11", "", ""], ["1", "3", "", ""], ["1", "50", "", ""], ["1", "8", "7", ""], ["1", "9", "3", ""], ["10", "", "", ""], ["10", "1", "", ""], ["100", "", "", ""], ["13", "", "", ""], ["2", "0", "", ""], ["2", "0", "0", ""], ["2", "0", "1", ""], ["2", "007", "", ""], ["2", "01", "", ""], ["2", "012b", "", ""], ["2", "01a", "", ""], ["2", "0a", "", ""], ["2", "0b", "", ""], ["2", "1", "", ""], ["25", "", "", ""], ["6", "", "", ""]]
-      if (a[0] <=> b[0]) == 0
-        if (a[1] <=> b[1]) == 0
-          a[2] <=> b[2]
-        else
-          a[0] <=> b[0]
-        end
-      else
-        a[1] <=> b[1]
-      end
-    end
-    p sorted_array
+  def initialize
+    
   end
+
+  def version_sort(orig_array)
+    array_to_sort = []
+    new_array = []
+
+    #get data from files
+    orig_array.each do |item|
+      array_to_sort << (/foo-(\d*)\.{0,1}(\d*[a-z]{0,1})\.{0,1}(\d*)\.ext/).match(item)
+    end
+
+    #sort array_to_sort
+    sorted = array_to_sort.sort do |array1, array2|
+      if (array1[1].to_i <=> array2[1].to_i) == 0
+      if (array1[2].to_i <=> array2[2].to_i) == 0
+        (array1[3].to_i <=> array2[3].to_i)
+      else
+        (array1[2].to_i <=> array2[2].to_i)
+      end
+      else
+        (array1[1].to_i <=> array2[1].to_i)
+      end
+    end
+
+    #add sorted file names to new array
+    sorted.each do |array|
+      new_array << array[0]
+    end
+
+    #return new array
+    p new_array
+  end
+
+
 
 end
 
