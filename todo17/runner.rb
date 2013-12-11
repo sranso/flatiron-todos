ed9b4590c6c243415ff1e0adf25dca2ff2cc502e
sranso <sranso@gmail.com>
2013-12-11 09:36:31 -0500

    fix some bugs

--- todo17/runner.rb
+++ todo17/runner.rb
@@ -1,11 +1,20 @@
 require_relative './jukebox.rb'
+require 'debugger'
 
 def run
   puts "Welcome to Ruby Console Jukebox!"
-  while command.downcase(command) != "exit" do
-    puts "Enter a command to continue. Type 'help' for a list of commands."
-    command = get_command
-    run_command(command) unless command.downcase == "exit"
+  puts "Enter a command to continue. Type 'help' for a list of commands."
+  command = get_command
+  while command.downcase != "exit" do
+    if ["help", "list", "u2", "talking heads", "huey lewis and the news"].include? command.downcase
+      # debugger
+      command = get_command
+      run_command(command) # unless command.downcase == "exit"
+    else
+      puts "I did not understand '#{command}'!"
+      puts "Enter a command to continue. Type 'help' for a list of commands."
+      command = get_command
+    end
   end
 end
 
@@ -27,7 +36,7 @@ def show_help
   help += "'help' - shows this menu\n"
   help += "'list' - lists the whole song library\n"
   help += "or you can enter an artist's name to show that artist's songs\n"
-  help = "or you can enter an artist's name to show that artist's songs\n"
+  help += "or you can enter an artist's name to show that artist's songs\n"
   puts help
 end
 
