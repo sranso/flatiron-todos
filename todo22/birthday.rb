7bf510f480c81233836dbebf15ce4f1f8a335f8c
sranso <sranso@gmail.com>
2013-12-18 09:03:27 -0500

    init commit todo22

---
+++ todo22/birthday.rb
@@ -0,0 +1,10 @@
+# create a sinatra application called "is it my birthday yet". it should have a single route that displays the word "yes" or "no" depending whether or not it is that day.
+ 
+# EXTENSION:
+# make a route that takes a parameter(the name of a holiday or event), and returns a single view that will display yes or no is it is that day.
+ 
+# INSPIRATION:
+# isitchristmas.com
+ 
+# ** while on that site make sure to check out the javascript console.
+
