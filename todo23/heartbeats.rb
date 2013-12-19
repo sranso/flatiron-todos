ece5f4276ecb6ec5dc166995a07a63616d96542e
sranso <sranso@gmail.com>
2013-12-19 09:21:50 -0500

    rename files, create class and it works

---
+++ todo23/heartbeats.rb
@@ -0,0 +1,21 @@
+class Heartbeats
+
+  def initialize
+    @beats = ["bum bum", "bam bam", "dun dun", "ba ba", "ba da"]
+  end
+
+  def each
+    @beats.each do |element|
+      yield element if block_given?
+    end
+  end
+
+  def first
+    @beats[0]
+  end
+
+  def [](num)
+    @beats[num]
+  end
+
+end
