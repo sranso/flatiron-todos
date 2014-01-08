77e06bd23da494e128c81e233ee28617257df4a9
sranso <sranso@gmail.com>
2014-01-08 09:13:27 -0500

    init comm todo31

---
+++ todo31/palindromes.rb
@@ -0,0 +1,8 @@
+# A palindrome is a string that is written the same forward as it is in reverse. Write a method to return the longest palindrome in a given string
+
+class String
+  def longest_palindrome
+    
+  end
+end
+"afbbbfjdjklgdfdhfdkjfffhhfffjkdfhdhkyejejfjkd".longest_palindrome #=> "dhfdkjfffhhfffjkdfhd"
