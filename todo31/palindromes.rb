ab6a54c90741461c7b8553b0b25a6cf9997357d6
sranso <sranso@gmail.com>
2014-01-08 13:11:45 -0500

    WE DID IT - SARAH AND GUS

--- todo31/palindromes.rb
+++ todo31/palindromes.rb
@@ -1,8 +1,30 @@
 # A palindrome is a string that is written the same forward as it is in reverse. Write a method to return the longest palindrome in a given string
+require "debugger"
 
 class String
+
   def longest_palindrome
-    
+    stage = []
+    pali = []
+    counter = 0
+    until counter == self.length
+      temp = []
+      self.split(//)[counter..-1].each do |char|
+        temp << char
+        fusion = temp.join("")
+        stage << fusion
+        stage.each do |word|
+          pali << word if word == word.reverse && word.length > 1
+        end
+      end
+      counter += 1
+    end
+    winner = "a"
+    pali.each do |contestant|
+      winner = contestant if contestant.length > winner.length
+    end
+    winner
   end
+
 end
-"afbbbfjdjklgdfdhfdkjfffhhfffjkdfhdhkyejejfjkd".longest_palindrome #=> "dhfdkjfffhhfffjkdfhd"
+p "afbbbfjdjklgdfdhfdkjfffhhfffjkdfhdhkyejejfjkd".longest_palindrome #=> "dhfdkjfffhhfffjkdfhd"
