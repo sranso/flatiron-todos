6b801d3de5360cae54a28ff505e69980684e0d24
sranso <sranso@gmail.com>
2014-01-14 09:21:58 -0500

    first part of method done

--- todo35/transpose.rb
+++ todo35/transpose.rb
@@ -1,11 +1,18 @@
 # Reimplement your own version of Ruby Array's transpose method
-
+require 'debugger'
 class Array
   def my_transpose
-    ___
+    new_array = []
+    self[0].each do |sub_array|
+      new_array << ([] << sub_array)
+    end
+    self[1..-1].each do |sub_array|
+      
+    end
+    new_array
   end
 end
 
-[[1, 2, 3], [:a, :b, :c]].my_transpose #=> [[1, :a], [2, :b], [3, :c]]
-[[1,2], [3,4], [5,6]].transpose #=> [[1, 3, 5], [2, 4, 6]]
-[].transpose #=> []
+p [[1, 2, 3], [:a, :b, :c]].my_transpose #=> [[1, :a], [2, :b], [3, :c]]
+# [[1,2], [3,4], [5,6]].transpose #=> [[1, 3, 5], [2, 4, 6]]
+# [].transpose #=> []
