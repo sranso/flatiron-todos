693fabdd28a0c9c3c1c03badbc2385eb3cc260d6
sranso <sranso@gmail.com>
2014-01-27 09:18:34 -0500

    init todo42

---
+++ todo42/first_even.js
@@ -0,0 +1,7 @@
+// prereqs: arrays, methods, conditional logic
+// Write a method that returns the first even element in a array of integers
+// This is an old todo we did in Ruby, please rewrite your answer in javascript
+
+function first_even(items) {
+  
+}
