240e0272ab5d6632ee2df51a27f2b1c3c7f453ea
sranso <sranso@gmail.com>
2014-01-27 09:22:48 -0500

    solve

--- todo42/first_even.js
+++ todo42/first_even.js
@@ -3,5 +3,9 @@
 // This is an old todo we did in Ruby, please rewrite your answer in javascript
 
 function first_even(items) {
-  
-}
+  for (var i = 0; i < items.length; i++) {
+    if (items[i] % 2 == 0) {
+      return items[i]
+    };
+  };
+};
